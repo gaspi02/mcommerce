4c55e0a874e2608f2076ff69d1182561f4980aae
Gaspi02 <duringer.gaspard@gmail.com>
2018-08-30 16:03:19 +0200

    test1

---
+++ clientui/src/main/java/com/clientui/clientui/exceptions/CustomErrorDecoder.java
@@ -0,0 +1,20 @@
+package com.clientui.clientui.exceptions;
+
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+
+public class CustomErrorDecoder implements ErrorDecoder {
+
+    private final ErrorDecoder defaulrErrorDecoder = new Default();
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+
+        if(response.status() == 404){
+
+            return new ProductNotFoundException("Produit non trouvé");
+        }
+        return defaulrErrorDecoder.decode(methodKey, response);
+    }
+}
